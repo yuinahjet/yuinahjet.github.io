038aa52dc87274967f1f5d313a02bf1e9c74b202
yuinahjet <gmathyuidr@gmail.com>
2016-05-14 16:10:16 +0900

    update

--- js/function.js
+++ js/function.js
@@ -17,7 +17,7 @@ function shuffle(array) {
     for (var i = 0; i < 15; i++) {
         var rand = Math.floor(Math.random() * array.length);
         console.log(array[rand]); //デバッグ
-  }
+    }
 
 }
 
@@ -25,17 +25,17 @@ function battle(me, you) {
 
   function draw(name) {
     var rand = Math.floor(Math.random() * deck.length);
-    name = deck[rand];
+    name.push(deck[rand]);
     deck.splice(rand, 1);
   }
     draw(me);
     draw(you);
 
-    if (me.num > you.num) {
+    if (me[0].num > you[0].num) {
         document.write("あなたの勝利です！");
-    } else if (me.num < you.num) {
+    } else if (me[0].num < you[0].num) {
         document.write("あなたの負けです…");
-    } else if (me.num == you.num) {
+    } else if (me[0].num == you[0].num) {
         document.write("引き分け");
     } else {
         document.write("おかしい");
--- js/indian.js
+++ js/indian.js
@@ -1,25 +1,13 @@
 var deck = [];
 var suit = ["HEART", "DIA", "CLUB", "SPADE"];
 
-deckPush(); //デッキをプッシュ
+deckPush(deck); //デッキをプッシュ
 
 
 
 var me = [];
 var you = [];
 
-draw(me);
-draw(you);
-//デバッグ用
-console.log(me);
-console.log(you);
-deckPush(deck); //デッキをプッシュ
-
-
-var me;
-var you;
-
-draw(me, you);
-// >>>>>>> 5f6707572bd7547a8d1c9cc7c6d71acc0ca55625
+battle(me, you, deck);
 
 shuffle(deck);
